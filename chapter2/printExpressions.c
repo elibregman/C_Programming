4d48becd1b7cc939f25a7d272f395cf4f1c1e731
Eli <bxr9979@hotmail.com>
2016-09-18 20:59:18 -0400

    using printf to print an expression

---
+++ chapter2/printExpressions.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /* using printf to print out complete expressions with logic inside of the function */
+
+    int x = 5, y = 5, z = 5;
+    printf("x * y * z = %d\n", x * y * z);
+    return 0;
+}
